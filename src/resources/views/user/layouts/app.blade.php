9e2ad0563efa8126fc7202bab7126e37184126b0
Daisuke70 <dafusongben52@gmail.com>
2025-04-30 15:50:43 +0900

    一般ユーザー、共通ヘッダー作成完了

--- src/resources/views/user/layouts/app.blade.php
+++ src/resources/views/user/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>勤怠管理アプリ</title>
+    <title>@yield('title', '勤怠管理アプリ')</title>
     <link rel="stylesheet" href="{{ asset('css/user/common.css')}}">
     @yield('css')
 </head>
@@ -12,33 +12,19 @@
         <header class="header">
 
         <div class="header-content">
-            <div class="header-logo">
+            <div class="header-content__logo">
                 <img src="{{ asset('/images/logo.svg') }}" alt="ロゴの画像" id="title" class="logo-img">
             </div>
-            <div class="search-form">
-                <form class="search-form" action="/" method="get">
+            <div class="header-content__link">
+                <a href="/attendance" class="header-link">勤怠</a>
+                <a href="/attendance/list" class="header-link">勤怠一覧</a>
+                <a href="/stamp_correction_request/list" class="header-link">申請</a>
+                <form class="header-content__logout-form" action="/logout" method="post">
                     @csrf
-                    <input type="text" name="keyword" class="keyword" placeholder="なにをお探しですか？" value="{{ request('keyword') }}">
-                    <input type="hidden" name="tab" value="{{ request('tab', 'recommended') }}">
+                    <button type="submit" class="logout-button">
+                        <span class="logout">ログアウト</span>
+                    </button>
                 </form>
-            </div>
-            <div class="logout-login">
-                @if (Auth::check())
-                    <form class="logout-form" action="/logout" method="post">
-                        @csrf
-                        <button type="submit" class="logout-button">
-                            <span class="logout">ログアウト</span>
-                        </button>
-                    </form>
-                @else
-                    <div class="login">
-                        <a href="/login" class="login-link">ログイン</a>
-                    </div>
-                @endif
-            </div>
-            <div class="link-content">
-                <a href="/mypage" class="mypage-link">マイページ</a>
-                <a href="/sell" class="sell-link">出品</a>
             </div>
         </div>
         </header>
